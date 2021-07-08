e21a3b95fa07b61d55a1df01a3778e9ada4aa7ab
HardikKheni <khenihardik1997@gmail.com>
2021-07-08 21:01:33 +0530

    2nd commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# laravel-vue-spa
+
+## Description
+
+AdminLTE port with laravel 8 and vue.js 3 (spa)
---
+++ webpack.mix.js
@@ -0,0 +1,26 @@
+const mix = require('laravel-mix');
+const path = require('path');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix
+	.js('resources/js/app/src/main.js', 'public/js')
+	.vue({ version: 3 })
+	.webpackConfig({
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, 'resources/js/app/src/'),
+			},
+		},
+	});
+// .postCss('resources/css/app.css', 'public/css', [
+// ]);
